708a19b9482c124aaef00d4bdb4c8e4973bbfd64
rresnic <reuv.resnicoff@gmail.com>
2024-07-23 17:31:50 +0300

    First set of XP Ninja exercises

---
+++ Week1/Day2/ExercisesXPNinja/exercise_5.py
@@ -0,0 +1,10 @@
+max_len = 0
+user_input = "Hello"
+while(user_input != "exit"):
+    user_input = input("Enter the longest sentence you can without the letter 'A': ")
+    if "A" or 'a' in user_input:
+        print("That didn't count! try one without an 'A'")
+    elif len(user_input) > max_len:
+        max_len = len(user_input)
+        print(f"Congratulations, your record is now {max_len}")
+print("Thanks for playing")
