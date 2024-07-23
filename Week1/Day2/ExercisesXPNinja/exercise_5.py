246d7f0ba92e75b75054a1d43a41b7094034c7a6
rresnic <reuv.resnicoff@gmail.com>
2024-07-23 17:33:22 +0300

    Edited Ninja exercise 5 to allow lowercase a

--- Week1/Day2/ExercisesXPNinja/exercise_5.py
+++ Week1/Day2/ExercisesXPNinja/exercise_5.py
@@ -2,7 +2,7 @@ max_len = 0
 user_input = "Hello"
 while(user_input != "exit"):
     user_input = input("Enter the longest sentence you can without the letter 'A': ")
-    if "A" or 'a' in user_input:
+    if "A" in user_input:
         print("That didn't count! try one without an 'A'")
     elif len(user_input) > max_len:
         max_len = len(user_input)
