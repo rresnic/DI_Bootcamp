f0066eb58dc050ff09c3db830d4770876c99080c
rresnic <reuv.resnicoff@gmail.com>
2024-08-06 18:32:35 +0300

    Added bonus

--- Week3/Day2/DailyChallenge/exercise.py
+++ Week3/Day2/DailyChallenge/exercise.py
@@ -1,3 +1,4 @@
+import random
 IDEAL_LENGTH = 10
 user_str = input("Enter a string: ")
 if len(user_str) == IDEAL_LENGTH:
@@ -6,6 +7,9 @@ if len(user_str) == IDEAL_LENGTH:
     for character in user_str:
         output_str += character
         print(output_str)
+    list_str = list(output_str)
+    random.shuffle(list_str)
+    print(''.join(list_str))
 elif len(user_str) > IDEAL_LENGTH:
     print("string too long")
 else:
