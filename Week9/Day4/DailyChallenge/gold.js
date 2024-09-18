6bb3e59a75164341dcf33ba87b9e041d42769c0c
rresnic <reuv.resnicoff@gmail.com>
2024-09-18 16:52:27 +0300

    Edited loop to make more efficient and more closely match the bubble sort algorithm

--- Week9/Day4/DailyChallenge/gold.js
+++ Week9/Day4/DailyChallenge/gold.js
@@ -9,12 +9,12 @@ console.log(numbersJoinStringPlus)
 let numbersJoinStringSpace = numbers.join(" ");
 console.log(numbersJoinStringSpace)
 
-for(let i = 0; i < numbers.length; i++){
-    for(let j = 0; j < numbers.length; j++){
-        if(numbers[i] > numbers[j]){
+for(let i = 0; i < numbers.length -1; i++){
+    for(let j = 0; j < numbers.length -1 - i; j++){
+        if(numbers[j] < numbers[j + 1]){
             let temp = numbers[j]
-            numbers[j] = numbers[i]
-            numbers[i] = temp
+            numbers[j] = numbers[j+1]
+            numbers[j+1] = temp
         }
     }
 }
