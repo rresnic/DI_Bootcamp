b93993c6fd219f4c5507a94b45a9c2fbca4542aa
rresnic <reuv.resnicoff@gmail.com>
2024-10-08 03:59:00 +0300

    Ninja

---
+++ Week11/Day6/ExerciseXPNinja/exercise1.js
@@ -0,0 +1,21 @@
+
+    // Analyze the code below, what will be the output?
+
+class Bird {
+  constructor() {
+    console.log("I'm a bird. 🦢");
+  }
+}
+
+class Flamingo extends Bird {
+  constructor() {
+    console.log("I'm pink. 🌸");
+    super();
+  }
+}
+
+const pet = new Flamingo();
+// "I'm pink. 🌸"
+// "I'm a bird. 🦢"
+
+// not particularly complicated?
